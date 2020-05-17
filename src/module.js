d614ece5a8c3ecf37efcf64b29c09f19261db237
Ilya Petrenko <gr0x562@gmail.com>
2020-05-17 19:50:34 +0300

    cleared files

--- src/module.js
+++
@@ -1,8 +0,0 @@
-console.log('module.js')
-
-// eslint-disable-next-line require-jsdoc
-async function start() {
-  return await Promise.resolve('test polyfill!')
-}
-
-start().then((res) => console.log(res))
