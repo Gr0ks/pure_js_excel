aa162ff8ef9565c5f62592dcdd767129371d7e56
Ilya Petrenko <gr0x562@gmail.com>
2020-05-20 22:30:28 +0300

    add generated main page from components

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,4 +1,9 @@
 /* eslint-disable require-jsdoc */
 export class DomListener {
-
+  constructor($root) {
+    if (!$root) {
+      throw new Error('No $root provided for DomListener!')
+    }
+    this.$root = $root
+  }
 }
