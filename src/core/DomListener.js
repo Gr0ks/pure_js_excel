68c902b1d141f7340107347a3b6c9ed02a3d5d04
Ilya Petrenko <gr0x562@gmail.com>
2020-05-24 16:43:15 +0300

    add listeners structure

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,9 +1,16 @@
 /* eslint-disable require-jsdoc */
 export class DomListener {
-  constructor($root) {
+  constructor($root, listeners = []) {
     if (!$root) {
       throw new Error('No $root provided for DomListener!')
     }
     this.$root = $root
+    this.listeners = listeners
   }
+
+  initDOMListeners() {
+    console.log(this.listeners)
+  }
+
+  removeDOMListeners() {}
 }
